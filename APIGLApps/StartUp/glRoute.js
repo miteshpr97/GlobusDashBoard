429cab0054b7ffd2b234809b1b2d1ec2a2461cf1
nawaid <hasantauqeer20@gmail.com>
2024-07-09 17:37:45 +0530

    Added authentication on all the route related to access right and user creation

--- APIGLApps/StartUp/glRoute.js
+++ APIGLApps/StartUp/glRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const glErrors = require("../CommonUtil/glErrors");
+const objVerify = require("../CommonUtil/authVerify")
 const apiLogin = require("../GLCM/GLCM1001");
 const apiMenu = require("../GLCM/GLCM1003");
 const apiAccessRight = require("../GLCM/GLCM1004");
@@ -10,7 +11,7 @@ module.exports = function (app) {
   app.use(express.json());
   app.use("/api/login", apiLogin);
   app.use("/api/Menu", apiMenu);
-  app.use("/api/AccessRight", apiAccessRight);
-  app.use("/api/UserCreation", apiUserCreation);
+  app.use("/api/AccessRight", objVerify, apiAccessRight);
+  app.use("/api/UserCreation", objVerify, apiUserCreation);
   app.use(glErrors);
 };
