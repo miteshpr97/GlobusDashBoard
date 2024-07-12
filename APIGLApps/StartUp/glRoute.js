287e10e1df021398867f2ca048b4f155a37eee85
AbdullahSam72 <muhammad.sayeed70@gmail.com>
2024-07-12 15:55:53 +0400

    commit

--- APIGLApps/StartUp/glRoute.js
+++ APIGLApps/StartUp/glRoute.js
@@ -11,7 +11,8 @@ module.exports = function (app) {
   app.use(express.json());
   app.use("/api/login", apiLogin);
   app.use("/api/Menu", apiMenu);
-  app.use("/api/AccessRight", apiAccessRight);
+  app.use("/api/AccessRight", objVerify, apiAccessRight);
   app.use("/api/UserCreation", objVerify, apiUserCreation);
+
   app.use(glErrors);
 };
