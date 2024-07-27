4355dc482d116cfb4febbc16b596f2c7b461f6b7
AbdullahSam72 <muhammad.sayeed70@gmail.com>
2024-07-27 15:25:42 +0400

    by saeed 20240727

--- APIGLApps/StartUp/glRoute.js
+++ APIGLApps/StartUp/glRoute.js
@@ -3,16 +3,19 @@ const glErrors = require("../CommonUtil/glErrors");
 const objVerify = require("../CommonUtil/authVerify");
 const apiLogin = require("../GLCM/GLCM1001");
 const apiMenu = require("../GLCM/GLCM1003");
-// const apiAccessRight = require("../GLCM/GLCM1004");
+
+const apiAccessRight = require("../GLCM/GLCM1004");
 const apiUserCreation = require("../GLCM/GLCMA100100");
-const apiAccessRight = require("../GLCM/GLCMA100200");
+const apiPageGLCMA100200 = require("../GLCM/GLCMA100200");
 
 require("express-async-errors");
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/login", apiLogin);
   app.use("/api/Menu", apiMenu);
-  app.use("/api/GLCMA100200", apiAccessRight);
+  app.use("/api/AccessRight", apiAccessRight);
+
   app.use("/api/UserCreation", apiUserCreation);
+  app.use("/api/GLCMA100200", apiPageGLCMA100200);
   app.use(glErrors);
 };
--- client/src/component/CommonComponnets/CommonBtn.js
+++ client/src/component/CommonComponnets/CommonBtn.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CustomButton from "../Buttons/CustomButton";
-import { Box, Container, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
-const CommonBtn = ({ PAGE_CD }) => {
+const CommonBtn = ({ PAGE_CD, SAVE_CLICK }) => {
   const [permissions, setPermissions] = useState(null);
   console.log(PAGE_CD, "page btn code");
 
   useEffect(() => {
     const fetchPermissions = async () => {
       try {
-        const response = await axios.post("/api/GLCMA100200/", {
+        const response = await axios.post("/api/AccessRight/", {
           USER_CD: window.sessionStorage.getItem("USER_CD"),
           // PAGE_CD: window.sessionStorage.getItem("PAGE_CD"),
           PAGE_CD: PAGE_CD,
@@ -19,9 +19,7 @@ const CommonBtn = ({ PAGE_CD }) => {
         if (response.status === 200) {
           const data = response.data;
           console.log(data, "dataatatt");
-          const pagePermissions = data.find(
-            (page) => page.PAGE_CD === window.sessionStorage.getItem("PAGE_CD")
-          );
+          const pagePermissions = data.find((page) => page.PAGE_CD === PAGE_CD);
           setPermissions(pagePermissions);
         }
       } catch (error) {
@@ -30,78 +28,84 @@ const CommonBtn = ({ PAGE_CD }) => {
     };
 
     fetchPermissions();
+    console.log("after fecth per");
   }, []);
 
   const handleClick = (action) => {
     alert(`${action} button clicked!`);
   };
 
+  //const SAVE_CLICK = () => {
+  // alert(`Save CLick button clicked!`);
+  // };
+
   if (!permissions) {
     return <div>Loading...</div>;
   }
 
   return (
-    <Box sx={{
-      width: "100%",
-      height: "60px",
-      background: "#dddddd",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "flex-start",
-      padding:"0px 10px"
-    }}>
+    <Box
+      sx={{
+        width: "100%",
+        height: "60px",
+        background: "#dddddd",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        padding: "0px 10px",
+      }}
+    >
+      <Stack direction="row" spacing={2}>
+        {permissions.PAGE_INQUIRY === "Y" && (
+          <CustomButton
+            variant="contained"
+            onClick={() => handleClick("INQUIRY")}
+            sx={{ backgroundColor: "#003285", color: "white" }}
+          >
+            INQUIRY
+          </CustomButton>
+        )}
 
-        <Stack direction="row" spacing={2}>
-          {permissions.PAGE_INQUIRY === "Y" && (
-            <CustomButton
-              variant="contained"
-              onClick={() => handleClick("INQUIRY")}
-              sx={{ backgroundColor: "#003285", color: "white" }}
-            > 
-              INQUIRY
-            </CustomButton>
-          )}
+        {permissions.PAGE_SAVE === "Y" && (
+          <CustomButton
+            variant="contained"
+            onClick={() => SAVE_CLICK()}
+            sx={{ backgroundColor: "#003285", color: "white" }}
+          >
+            SAVE
+          </CustomButton>
+        )}
 
-          {permissions.PAGE_SAVE === "Y" && (
-            <CustomButton
-              variant="contained"
-              onClick={() => handleClick("SAVE")}
-              sx={{ backgroundColor: "#003285", color: "white" }}
-            >
-              SAVE
-            </CustomButton>
-          )}
+        {permissions.PAGE_UPDATE === "Y" && (
+          <CustomButton
+            variant="contained"
+            onClick={() => handleClick("UPDATE")}
+            sx={{ backgroundColor: "#7E8EF1", color: "white" }}
+          >
+            UPDATE
+          </CustomButton>
+        )}
 
-          {permissions.PAGE_UPDATE === "Y" && (
-            <CustomButton
-              variant="contained"
-              onClick={() => handleClick("UPDATE")}
-              sx={{ backgroundColor: "#7E8EF1", color: "white" }}
-            >
-              UPDATE
-            </CustomButton>
-          )}
+        {permissions.PAGE_DELETE === "Y" && (
+          <CustomButton
+            variant="contained"
+            onClick={() => handleClick("DELETE")}
+            sx={{ backgroundColor: "#A1C398", color: "white" }}
+          >
+            DELETE
+          </CustomButton>
+        )}
 
-          {permissions.PAGE_DELETE === "Y" && (
-            <CustomButton
-              variant="contained"
-              onClick={() => handleClick("DELETE")}
-              sx={{ backgroundColor: "#A1C398", color: "white" }}
-            >
-              DELETE
-            </CustomButton>
-          )}
-
-          {permissions.PAGE_EXCEL === "Y" && (
-            <CustomButton
-              variant="contained"
-              onClick={() => handleClick("EXCEL")}
-              sx={{ backgroundColor: "#704264", color: "white" }}
-            >
-              EXCEL
-            </CustomButton>
-          )}
-        </Stack>
+        {permissions.PAGE_EXCEL === "Y" && (
+          <CustomButton
+            variant="contained"
+            onClick={() => handleClick("EXCEL")}
+            sx={{ backgroundColor: "#704264", color: "white" }}
+          >
+            EXCEL
+          </CustomButton>
+        )}
+      </Stack>
     </Box>
   );
 };
