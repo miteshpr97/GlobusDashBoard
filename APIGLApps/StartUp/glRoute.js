064220b4bba6568ef3e6756071a670c8342d8026
nawaid <hasantauqeer20@gmail.com>
2024-07-29 12:15:19 +0530

    Updated code for custom buttons based on access

--- APIGLApps/StartUp/glRoute.js
+++ APIGLApps/StartUp/glRoute.js
@@ -3,7 +3,7 @@ const glErrors = require("../CommonUtil/glErrors");
 const objVerify = require("../CommonUtil/authVerify")
 const apiLogin = require("../GLCM/GLCM1001");
 const apiMenu = require("../GLCM/GLCM1003");
-// const apiAccessRight = require("../GLCM/GLCM1004");
+const apiPageAccessRight = require("../GLCM/GLCM1004");
 const apiUserCreation = require("../GLCM/GLCMA100100");
 const apiAccessRight = require("../GLCM/GLCMA100200");
 
@@ -12,6 +12,7 @@ module.exports = function (app) {
   app.use(express.json());
   app.use("/api/login", apiLogin);
   app.use("/api/Menu", apiMenu);
+  app.use("/api/PageAccessRight", apiPageAccessRight);
   app.use("/api/GLCMA100200", apiAccessRight);
   app.use("/api/UserCreation", apiUserCreation);
   app.use(glErrors);
