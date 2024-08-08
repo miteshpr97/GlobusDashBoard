0d10e999bbd31a1a6110315452859ae2ecf92225
AbdullahSam72 <muhammad.sayeed70@gmail.com>
2024-08-09 00:06:27 +0400

    2nd

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,30 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import Login from './auth/Login/Login';
-import { Dashboard } from './Pages/Dashboard/Dashboard';
+import Login from "./auth/Login/Login";
+import { Dashboard } from "./Pages/Dashboard/Dashboard";
 
-import GLCMA100200 from './Pages/GLCMA100200/GLCMA100200';
-import GLCMA100100 from './Pages/GLCMA100100/GLCMA100100';
-import GLCMA100300 from './Pages/GLCMA100300/GLCMA100300';
-import GLAMAT001001 from './Pages/GLAMAT001001/GLAMAT001001';
-
-
+import GLCMA100200 from "./Pages/GLCMA100200/GLCMA100200";
+import GLCMA100100 from "./Pages/GLCMA100100/GLCMA100100";
+import GLCMA100300 from "./Pages/GLCMA100300/GLCMA100300";
+import GLAMAT001001 from "./Pages/GLAMT100100/GLAMT100100";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard/>}></Route>   
-        <Route path='/UserCreation' element = {<GLCMA100100/>}></Route>
-        <Route path='/UserAccess' element = {<GLCMA100200/>}></Route>
-        <Route path='/CommonCode' element = {<GLCMA100300/>}></Route>
-        <Route path='/GLAMAT001001' element = {<GLAMAT001001/>}></Route>
-        
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/UserCreation" element={<GLCMA100100 />}></Route>
+        <Route path="/UserAccess" element={<GLCMA100200 />}></Route>
+        <Route path="/CommonCode" element={<GLCMA100300 />}></Route>
+        <Route path="/GLAMAT001001" element={<GLAMAT001001 />}></Route>
       </Routes>
-
     </>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // import './App.css';
 // import { Routes, Route } from "react-router-dom";
@@ -92,8 +57,7 @@ export default App;
 //           </>
 //         }
 //       />
-        
-       
+
 //       </Routes>
 
 //     </>
