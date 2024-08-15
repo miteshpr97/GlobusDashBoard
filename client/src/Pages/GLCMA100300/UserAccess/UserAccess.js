a8b2ad977ed291c7d6b577ede50a0f45bda3a951
miteshpr97 <miteshpradhan97@gmail.com>
2024-08-15 22:56:29 +0530

    working on common page

--- client/src/Pages/GLCMA100300/UserAccess/UserAccess.js
+++ client/src/Pages/GLCMA100300/UserAccess/UserAccess.js
@@ -8,6 +8,9 @@ import {
   TableRow,
   Paper,
   TablePagination,
+  TextField,
+  Grid,
+  Button,
 } from "@mui/material";
 import axios from "axios";
 
@@ -16,9 +19,13 @@ const UserAccess = ({ selectedModule }) => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
 
+
+  console.log(moduleData, "data");
+  
+
   useEffect(() => {
     const fetchModuleData = async () => {
-      if (!selectedModule) return; 
+      if (!selectedModule) return;
 
       try {
         const response = await axios.post("/api/GLCMA100300/data", {
@@ -37,7 +44,7 @@ const UserAccess = ({ selectedModule }) => {
     };
 
     fetchModuleData();
-  }, [selectedModule]); 
+  }, [selectedModule]);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -46,6 +53,26 @@ const UserAccess = ({ selectedModule }) => {
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
+  };
+
+  const handleInputChange = (index, field, value) => {
+    const updatedModuleData = [...moduleData];
+    updatedModuleData[index][field] = value;
+    setModuleData(updatedModuleData);
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const response = await axios.post("/api/GLCMA100300/", moduleData);
+
+      if (response.status === 200) {
+        console.log("Data updated successfully!");
+      } else {
+        console.error("Failed to update data:", response.data);
+      }
+    } catch (error) {
+      console.error("Error updating data:", error);
+    }
   };
 
   const emptyRows =
@@ -77,22 +104,214 @@ const UserAccess = ({ selectedModule }) => {
             {moduleData
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((module, index) => (
-                <TableRow key={index}>               
-                  <TableCell>{module.M_DVN}</TableCell>
-                  <TableCell>{module.C_DVN}</TableCell>
-                  <TableCell>{module.CODE_NO}</TableCell>
-                  <TableCell>{module.CODE_NM}</TableCell>
-                  <TableCell>{module.CODE_NMH}</TableCell>
-                  <TableCell>{module.CODE_NMA}</TableCell>
-                  <TableCell>{module.CODE_NMO}</TableCell>
-                  <TableCell>{module.SUB_GUN1}</TableCell>
-                  <TableCell>{module.SUB_GUN2}</TableCell>
-                  <TableCell>{module.SUB_GUN3}</TableCell>
-                  <TableCell>{module.SUB_GUN4}</TableCell>
-                  <TableCell>{module.SUB_GUN5}</TableCell>
-                  <TableCell>{module.ORDER_NO}</TableCell>
-                  <TableCell>{module.SORT_BY}</TableCell>
-                  <TableCell>{module.RMKS}</TableCell>
+                <TableRow key={index}>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.M_DVN}
+                        onChange={(e) =>
+                          handleInputChange(index, "M_DVN", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+
+                        fullWidth
+                        inputProps={
+                          { readOnly: true, }
+                        }
+
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.C_DVN}
+                        onChange={(e) =>
+                          handleInputChange(index, "C_DVN", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        inputProps={
+                          { readOnly: true, }
+                        }
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.CODE_NO}
+                        onChange={(e) =>
+                          handleInputChange(index, "CODE_NO", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        inputProps={
+                          { readOnly: true, }
+                        }
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.CODE_NM}
+                        onChange={(e) =>
+                          handleInputChange(index, "CODE_NM", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.CODE_NMH}
+                        onChange={(e) =>
+                          handleInputChange(index, "CODE_NMH", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.CODE_NMA}
+                        onChange={(e) =>
+                          handleInputChange(index, "CODE_NMA", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.CODE_NMO}
+                        onChange={(e) =>
+                          handleInputChange(index, "CODE_NMO", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.SUB_GUN1}
+                        onChange={(e) =>
+                          handleInputChange(index, "SUB_GUN1", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.SUB_GUN2}
+                        onChange={(e) =>
+                          handleInputChange(index, "SUB_GUN2", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.SUB_GUN3}
+                        onChange={(e) =>
+                          handleInputChange(index, "SUB_GUN3", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.SUB_GUN4}
+                        onChange={(e) =>
+                          handleInputChange(index, "SUB_GUN4", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.SUB_GUN5}
+                        onChange={(e) =>
+                          handleInputChange(index, "SUB_GUN5", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.ORDER_NO}
+                        onChange={(e) =>
+                          handleInputChange(index, "ORDER_NO", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.SORT_BY}
+                        onChange={(e) =>
+                          handleInputChange(index, "SORT_BY", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <Grid item xs={12} style={{ width: "100px" }}>
+                      <TextField
+                        value={module.RMKS}
+                        onChange={(e) =>
+                          handleInputChange(index, "RMKS", e.target.value)
+                        }
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                      />
+                    </Grid>
+                  </TableCell>
                 </TableRow>
               ))}
             {emptyRows > 0 && (
@@ -112,9 +331,165 @@ const UserAccess = ({ selectedModule }) => {
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
+
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleUpdate}
+        style={{ marginTop: "16px" }}
+      >
+        Update
+      </Button>
     </>
   );
 };
 
 export default UserAccess;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import {
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableContainer,
+//   TableHead,
+//   TableRow,
+//   Paper,
+//   TablePagination,
+//   TextField,
+//   Grid,
+// } from "@mui/material";
+// import axios from "axios";
+
+// const UserAccess = ({ selectedModule }) => {
+//   const [moduleData, setModuleData] = useState([]);
+//   const [page, setPage] = useState(0);
+//   const [rowsPerPage, setRowsPerPage] = useState(10);
+
+//   useEffect(() => {
+//     const fetchModuleData = async () => {
+//       if (!selectedModule) return;
+
+//       try {
+//         const response = await axios.post("/api/GLCMA100300/data", {
+//           MODULE_CD: selectedModule.M_DVN,
+//           CODE_NO: selectedModule.CODE_NO,
+//         });
+
+//         if (response.status === 200 && Array.isArray(response.data)) {
+//           setModuleData(response.data);
+//         } else {
+//           console.error("Unexpected response data:", response.data);
+//         }
+//       } catch (error) {
+//         console.error("Error fetching module data:", error);
+//       }
+//     };
+
+//     fetchModuleData();
+//   }, [selectedModule]);
+
+//   const handleChangePage = (event, newPage) => {
+//     setPage(newPage);
+//   };
+
+//   const handleChangeRowsPerPage = (event) => {
+//     setRowsPerPage(+event.target.value);
+//     setPage(0);
+//   };
+
+//   const handleInputChange = (index, field, value) => {
+//     const updatedModuleData = [...moduleData];
+//     updatedModuleData[index][field] = value;
+//     setModuleData(updatedModuleData);
+//   };
+
+//   const emptyRows =
+//     rowsPerPage - Math.min(rowsPerPage, moduleData.length - page * rowsPerPage);
+
+//   return (
+//     <>
+//       <TableContainer component={Paper} sx={{ width: "100%", maxHeight: 440 }}>
+//         <Table stickyHeader aria-label="sticky table">
+//           <TableHead>
+//             <TableRow style={{ backgroundColor: "#f5f5f5" }}>
+//               <TableCell style={{ fontWeight: "bold" }}>Module</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>Code DVN</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>Code NO</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>Code NM</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>Code NMH</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>Code NMA</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>Code NMO</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>SUB_GUN 1</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>SUB_GUN 2</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>SUB_GUN 3</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>SUB_GUN 4</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>SUB_GUN 5</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>SORT_BY</TableCell>
+//               <TableCell style={{ fontWeight: "bold" }}>RMKS</TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {moduleData
+//               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+//               .map((module, index) => (
+//                 <TableRow key={index}>
+//                   {Object.keys(module).map((field) => (
+//                     <TableCell key={field}>
+//                       <Grid container spacing={1}>
+//                         <Grid item xs={12} style={{ width:"100px"}}>
+//                           <TextField
+//                             value={module[field]}
+//                             onChange={(e) =>
+//                               handleInputChange(index, field, e.target.value)
+//                             }
+//                             variant="outlined"
+//                             size="small"
+//                             fullWidth
+//                           />
+//                         </Grid>
+//                       </Grid>
+//                     </TableCell>
+//                   ))}
+//                 </TableRow>
+//               ))}
+//             {emptyRows > 0 && (
+//               <TableRow style={{ height: 53 * emptyRows }}>
+//                 <TableCell colSpan={14} />
+//               </TableRow>
+//             )}
+//           </TableBody>
+//         </Table>
+//       </TableContainer>
+//       <TablePagination
+//         rowsPerPageOptions={[10, 25, 100]}
+//         component="div"
+//         count={moduleData.length}
+//         rowsPerPage={rowsPerPage}
+//         page={page}
+//         onPageChange={handleChangePage}
+//         onRowsPerPageChange={handleChangeRowsPerPage}
+//       />
+//     </>
+//   );
+// };
+
+// export default UserAccess;
